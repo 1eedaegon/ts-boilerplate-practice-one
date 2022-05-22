eaefc89f3e08777e4a45d34a1ad23aa150cfaa68
1eedaegon <d8726243@gmail.com>
2022-05-22 20:23:40 +0900

    Initialize boilerplate

---
+++ jest.config.ts
@@ -0,0 +1,55 @@
+import path from 'path';
+const rootDirectory = path.resolve(__dirname)
+
+export default {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory:'coverage',
+    coverageProvider:'v8',
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            function: 80,
+            lines: 80,
+            statements: 80,
+        }
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+        },
+    },
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '@server(.*)$': `${rootDirectory}/src$1`,
+        '@config(.*)$': `${rootDirectory}/src/config$1`,
+        '@tests(.*)$': `${rootDirectory}/__tests__$1`,
+        '@domain(.*)$': `${rootDirectory}/src/domain$1`,
+        '@controller(.*)$': `${rootDirectory}/src/controller$1`,
+        '@middleware(.*)$': `${rootDirectory}/src/middleware$1`, 
+    },
+    reporters: [
+        'default',
+        [
+            path.resolve(__dirname, 'node_modules', 'jest-html-reporter'),
+            {
+                pageTitle: 'Demo test report',
+                outputPath: 'test-report.html',
+            }
+        ],
+    ],
+    rootDir: rootDirectory,
+    roots: [rootDirectory],
+    setupFilesAfterEnv: [`${rootDirectory}/__tests__/setup.ts`],
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '<rootDir>/build',
+        `${rootDirectory}/__tests__/fixtures`,
+        `${rootDirectory}/__tests__/setup.ts`
+    ],
+    transform: {
+        '^.+\\.ts$': 'ts-test'
+    },
+    testRegex: ['((/__tests__/.*)|(\\.|/)(test|spec))\\.tsx?$'],
+}
