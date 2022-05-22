34322c1b7db33e7c9a9a1a1651457798116d39ff
1eedaegon <d8726243@gmail.com>
2022-05-22 21:27:21 +0900

    Initialize express environment

---
+++ src/config/express.ts
@@ -0,0 +1,22 @@
+import locationRouter from "@controller/location/location.router"
+import errorHandler from "@middleware/errorHandler"
+import express from "express"
+import cors from 'cors'
+
+const createServer  = (): express.Application => {
+    const app = express();
+    app.use(express.urlencoded({ extended: true }))
+    app.use(cors());
+    app.use(express.json());
+    app.disable('x-powered-by')
+    app.get('/health', (_req, res) => {
+        res.send('UP')
+    });
+
+    app.use('/', locationRouter)
+    app.use(errorHandler)
+
+    return app;
+}
+
+export { createServer };
